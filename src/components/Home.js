003f8e28a96c6d57575fcddc2077464c6c445ae5
Jerome Taruc <jerometaruc29@gmail.com>
2025-04-10 16:04:47 -0700

    Updated formatting

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ const Home = () => {
     return (
         <section id="home" className="section">
             <div className="content">
-                <h2>Welcome to My Portfolio</h2>
-                <p>This is the home section. Here you'll find an introduction to who I am and what I do.</p>
+                <h1>Hello World, I'm <b>Jerome</b>.</h1>
+                <p>Welcome to V2.0 of my Portfolio Website</p>
             </div>
         </section>
     );
